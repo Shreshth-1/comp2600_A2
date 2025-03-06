d11bf46ff0847d3f8fcc019f3b41e3a98cb28607
Shreshth-1 <shresthtyagi2004@gmail.com>
2025-03-06 10:25:14 -0600

    Saving local changes before pulling

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,7 @@
 AUTHOR = 'Shreshth Tyagi'
 SITENAME = 'My Resume'
 SITEURL = ""
+THEME = "themes/Flex"
 
 PATH = "content"
 
@@ -16,18 +17,10 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
+LINKS = ()
 
 # Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
-)
+SOCIAL = ()
 
 DEFAULT_PAGINATION = 10
 
